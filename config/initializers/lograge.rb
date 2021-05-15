3c56f8068a749aa281c55e4a3e34e5e8e02d9fb7
shgtkshruch <shgtk.shruch@gmail.com>
2021-05-15 21:03:28 +0900

    Logging ip address and params

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,3 +1,10 @@
 Rails.application.configure do
   config.lograge.enabled = true
+
+  config.lograge.custom_options = lambda do |event|
+    {
+      ip: event.payload[:ip],
+      params: event.payload[:params]
+    }
+  end
 end
