4c3ea39eed2032ac3f2158dc3df71181709a6f23
shgtkshruch <shgtk.shruch@gmail.com>
2021-05-28 23:50:39 +0900

    Refactoring

--- scripts/switch-maintenance.sh
+++ scripts/switch-maintenance.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env bash
 
-vpc_id=$(copilot env show --name dev --json | jq '.environmentVPC.id')
+set -e -u
 
+env=$1
+
+FIRST_PRIORITY=1
+SECOND_PRIORITY=50000
+
+vpc_id=$(copilot env show --name $env --json | jq '.environmentVPC.id')
 elb=$(aws elbv2 describe-load-balancers | jq ".LoadBalancers[] | select(.VpcId == $vpc_id)" | jq -r ".LoadBalancerArn")
+listener=$(aws elbv2 describe-listeners --load-balancer-arn $elb | jq -r '.Listeners[].ListenerArn')
 
-lisner=$(aws elbv2 describe-listeners --load-balancer-arn $elb | jq -r '.Listeners[].ListenerArn')
-
-rule_first=$(aws elbv2 describe-rules --listener-arn $lisner | jq -r '.Rules[] |  select(.Priority == "1") | .RuleArn')
-rule_second=$(aws elbv2 describe-rules --listener-arn $lisner | jq -r '.Rules[] |  select(.Priority == "50000") | .RuleArn')
+rules=$(aws elbv2 describe-rules --listener-arn $listener)
+first_rule=$(echo $rules | jq -r '.Rules[] | select(.Priority == "'"$FIRST_PRIORITY"'") | .RuleArn')
+second_rule=$(echo $rules | jq -r '.Rules[] | select(.Priority == "'"$SECOND_PRIORITY"'") | .RuleArn')
 
 rules=$(aws elbv2 set-rule-priorities --rule-priorities '[
   {
-    "RuleArn": "'"$rule_first"'",
-    "Priority": 50000
+    "RuleArn": "'"$first_rule"'",
+    "Priority": '$SECOND_PRIORITY'
   },
   {
-    "RuleArn": "'"$rule_second"'",
-    "Priority": 1
+    "RuleArn": "'"$second_rule"'",
+    "Priority": '$FIRST_PRIORITY'
   }
 ]')
 echo $rules
