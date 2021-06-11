06604fa7cf8b0203738b36c82e98878e489a562b
shgtkshruch <shgtk.shruch@gmail.com>
2021-06-12 07:20:30 +0900

    Disable colorize logging
    https://docs.datadoghq.com/ja/logs/log_collection/ruby/

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -2,6 +2,9 @@ Rails.application.configure do
   config.lograge.enabled = true
   config.lograge.formatter = Lograge::Formatters::Json.new
 
+  ## Disables log coloration
+  config.colorize_logging = false
+
   # Exclude heathcheck logs from rails logs
   # https://github.com/linqueta/rails-healthcheck#ignoring-logs
   config.lograge.ignore_actions = [Healthcheck::CONTROLLER_ACTION]
