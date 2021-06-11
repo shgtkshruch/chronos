229fba4fbce454079610fd8657be75ee302f3103
shgtkshruch <shgtk.shruch@gmail.com>
2021-06-12 07:16:44 +0900

    Remove duplicate settings from lograge

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,7 +1,6 @@
 Rails.application.configure do
   config.lograge.enabled = true
   config.lograge.formatter = Lograge::Formatters::Json.new
-  config.lograge.ignore_actions = [Healthcheck::CONTROLLER_ACTION]
 
   # Exclude heathcheck logs from rails logs
   # https://github.com/linqueta/rails-healthcheck#ignoring-logs
