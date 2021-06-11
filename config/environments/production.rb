007c322d56d813b940a7c74ea229df6aeb7027fe
shgtkshruch <shgtk.shruch@gmail.com>
2021-06-12 07:15:54 +0900

    Move trace id settings to `config/environments`
    https://docs.datadoghq.com/ja/tracing/connect_logs_and_traces/ruby/#ruby-%E3%82%A2%E3%83%97%E3%83%AA%E3%82%B1%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3%E3%81%AB%E3%83%AD%E3%82%AE%E3%83%B3%E3%82%B0%E3%81%99%E3%82%8B%E5%A0%B4%E5%90%88

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -125,4 +125,25 @@ Rails.application.configure do
     span.name == 'rack.request' && span.get_tag('http.url') == Healthcheck.configuration.route
   end
   Datadog::Pipeline.before_flush(filter)
+
+  # Lograge
+  # 現在のスレッドのトレース情報を取得
+  # https://docs.datadoghq.com/ja/tracing/connect_logs_and_traces/ruby/#ruby-%E3%82%A2%E3%83%97%E3%83%AA%E3%82%B1%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3%E3%81%AB%E3%83%AD%E3%82%AE%E3%83%B3%E3%82%B0%E3%81%99%E3%82%8B%E5%A0%B4%E5%90%88
+  config.lograge.custom_options = lambda do |event|
+    correlation = Datadog.tracer.active_correlation
+
+    {
+      ip: event.payload[:ip],
+      # https://docs.datadoghq.com/ja/tracing/connect_logs_and_traces/ruby/
+      dd: {
+        trace_id: correlation.trace_id.to_s,
+        span_id: correlation.span_id.to_s,
+        env: correlation.env.to_s,
+        service: correlation.service.to_s,
+        version: correlation.version.to_s
+      },
+      ddsource: ["ruby"],
+      params: event.payload[:params]
+    }
+  end
 end
--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -3,24 +3,6 @@ Rails.application.configure do
   config.lograge.formatter = Lograge::Formatters::Json.new
   config.lograge.ignore_actions = [Healthcheck::CONTROLLER_ACTION]
 
-  # 現在のスレッドのトレース情報を取得
-  correlation = Datadog.tracer.active_correlation
-  config.lograge.custom_options = lambda do |event|
-    {
-      ip: event.payload[:ip],
-      # https://docs.datadoghq.com/ja/tracing/connect_logs_and_traces/ruby/
-      dd: {
-        trace_id: correlation.trace_id.to_s,
-        span_id: correlation.span_id.to_s,
-        env: correlation.env.to_s,
-        service: correlation.service.to_s,
-        version: correlation.version.to_s
-      },
-      ddsource: ["ruby"],
-      params: event.payload[:params]
-    }
-  end
-
   # Exclude heathcheck logs from rails logs
   # https://github.com/linqueta/rails-healthcheck#ignoring-logs
   config.lograge.ignore_actions = [Healthcheck::CONTROLLER_ACTION]
